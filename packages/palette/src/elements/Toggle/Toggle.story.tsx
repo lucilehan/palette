b795fedc5474ef195e7865ff800742a33517e8c0
dzucconi <mail@damonzucconi.com>
2021-02-11 17:23:47 -0500

    chore(storybook): runs codemod to convert to csf format

--- packages/palette/src/elements/Toggle/Toggle.story.tsx
+++ packages/palette/src/elements/Toggle/Toggle.story.tsx
@@ -1,4 +1,3 @@
-import { storiesOf } from "@storybook/react"
 import React from "react"
 import { Box } from "../Box"
 import { Link } from "../Link"
@@ -7,7 +6,7 @@ import { Toggle } from "./Toggle"
 const SecondaryAction = () => {
   return (
     <Link
-      onClick={e => {
+      onClick={(e) => {
         alert("hello world!")
         e.stopPropagation()
       }}
@@ -17,45 +16,63 @@ const SecondaryAction = () => {
   )
 }
 
-storiesOf("Components/Toggle", module)
-  .add("Toggle", () => {
-    return (
-      <Box width="350px">
-        <Toggle label="Test" expanded>
-          <h1>Hello world</h1>
-        </Toggle>
-      </Box>
-    )
-  })
-  .add("Toggle disabled", () => {
-    return (
-      <Box width="350px">
-        <Toggle label="Test" expanded disabled>
-          <h1>Hello world</h1>
-        </Toggle>
-      </Box>
-    )
-  })
-  .add("Toggle with secondary action", () => {
-    return (
-      <Box width="350px">
-        <Toggle label="Test" expanded renderSecondaryAction={SecondaryAction}>
-          <h1>Hello world</h1>
-        </Toggle>
-      </Box>
-    )
-  })
-  .add("Toggle disabled with secondary action", () => {
-    return (
-      <Box width="350px">
-        <Toggle
-          label="Test"
-          expanded
-          disabled
-          renderSecondaryAction={SecondaryAction}
-        >
-          <h1>Hello world</h1>
-        </Toggle>
-      </Box>
-    )
-  })
+export default {
+  title: "Components/Toggle",
+}
+
+export const _Toggle = () => {
+  return (
+    <Box width="350px">
+      <Toggle label="Test" expanded>
+        <h1>Hello world</h1>
+      </Toggle>
+    </Box>
+  )
+}
+
+export const ToggleDisabled = () => {
+  return (
+    <Box width="350px">
+      <Toggle label="Test" expanded disabled>
+        <h1>Hello world</h1>
+      </Toggle>
+    </Box>
+  )
+}
+
+ToggleDisabled.story = {
+  name: "Toggle disabled",
+}
+
+export const ToggleWithSecondaryAction = () => {
+  return (
+    <Box width="350px">
+      <Toggle label="Test" expanded renderSecondaryAction={SecondaryAction}>
+        <h1>Hello world</h1>
+      </Toggle>
+    </Box>
+  )
+}
+
+ToggleWithSecondaryAction.story = {
+  name: "Toggle with secondary action",
+}
+
+export const ToggleDisabledWithSecondaryAction = () => {
+  return (
+    <Box width="350px">
+      <Toggle
+        label="Test"
+        expanded
+        disabled
+        renderSecondaryAction={SecondaryAction}
+      >
+        <h1>Hello world</h1>
+      </Toggle>
+    </Box>
+  )
+}
+
+ToggleDisabledWithSecondaryAction.story = {
+  name: "Toggle disabled with secondary action",
+}
